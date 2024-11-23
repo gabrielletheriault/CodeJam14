199158ffdddc81d41964c5a4ca1d1f527c181e91
Ruben Hari <rhari@Rubens-MacBook-Air.local>
2024-11-23 14:59:44 -0500

    Version 1 of Bar Graph

--- src/app/graphs/page.tsx
+++ src/app/graphs/page.tsx
@@ -1,5 +1,12 @@
-const Page = () => {
+import { BarChartHero } from "@/components/BarChartHero"
 
+const Page = () => {
+    return (
+        <div>
+            <h1>Bar Chart Page</h1>
+            <BarChartHero />
+        </div>
+    )
 }
 
 export default Page
