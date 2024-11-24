24c71cc5f01472603c1cb14f93eaf91e944e73de
Josh Bowman <joshabowmanicloud.com@Joshs-MacBook-Pro.local>
2024-11-24 10:25:53 -0500

    UI Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,58 +45,56 @@ const Page = () => {
     }
 
   return (
-  <>
-      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0 font-mono">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-              <div className="flex flex-col items-center space-y-2 text-center">
-                  <h1 className="text-2xl font-bold">
-                      Register Account
-                  </h1>
+    <div className="absolute inset-0 flex flex-col items-center justify-center font-mono">
+        <div className="mx-auto flex w-full max-w-sm flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col items-center space-y-2 text-center">
+                <h1 className="text-2xl font-bold">Register Account</h1>
 
-                  <Link className={buttonVariants({
-                      variant: "link",
-                      className: "gap-1.5"
-                  })} href="/sign-in">
-                      Already have an account?
-                      <ArrowRight className="h-4 w-4"/>
-                  </Link>
-              </div>
+                <Link
+                    className={buttonVariants({
+                        variant: "link",
+                        className: "gap-1.5",
+                    })}
+                    href="/sign-in"
+                >
+                    Already have an account?
+                    <ArrowRight className="h-4 w-4" />
+                </Link>
+            </div>
 
-              <div className="grid gap-6">
-                  <form onSubmit={handleSubmit}>
-                      <div className="grid gap-2">
-                          <div className="grid gap-1 py-2">
-                              <Label htmlFor="email">Email</Label>
-                              <Input
-                                  className="border mx-2 border-gray-500 rounded"
-                                  type="email"
-                                  name="email"
-                                  id="email"
-                                  placeholder="email"
-                              />
-                          </div>
+            <div className="grid gap-6">
+                <form onSubmit={handleSubmit}>
+                    <div className="grid gap-4">
+                        <div className="grid gap-1">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                className="border border-gray-500 rounded"
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder="email"
+                            />
+                        </div>
 
-                          <div className="grid gap-1 py-2">
-                              <Label htmlFor="password">Password</Label>
-                              <Input
-                                  className="border mx-2 border-gray-500 rounded"
-                                  type="password"
-                                  name="password"
-                                  id="password"
-                                  placeholder="Password"
-                              />
+                        <div className="grid gap-1">
+                            <Label htmlFor="password">Password</Label>
+                            <Input
+                                className="border border-gray-500 rounded"
+                                type="password"
+                                name="password"
+                                id="password"
+                                placeholder="Password"
+                            />
+                        </div>
 
-                          </div>
+                        <Button>Register</Button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
-                          <Button>Register</Button>
-                      </div>
-                  </form>
-
-                
-              </div>
-          </div>
-      </div>
-  </>)
+    )
 }
 
 export default Page
