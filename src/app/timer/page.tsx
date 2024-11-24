e864b9d0919e1b162ee4a0eb23fb5c451a391774
Josh Bowman <joshabowmanicloud.com@Joshs-MacBook-Pro.local>
2024-11-24 11:34:06 -0500

    TickTrack

--- src/app/timer/page.tsx
+++ src/app/timer/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
        
        <h1 className={`text-7xl font-bold tracking-tight font-mono`}> 
-          Watch Me Cook
+          TickTrack
         </h1>
        
         <div style={{ marginTop: "120px" }}></div>
